bfdc53204a1f36432989e94e95430845ab39f76c
Somon Gulmamadov <90386011+Pomiray92@users.noreply.github.com>
2023-06-05 15:33:49 +0200

    setting 'making_pdf2.py' as main with seccessfull connect to db getting the data and creating a simple pdf from it

--- making_pdf2.py
+++ making_pdf2.py
@@ -1,34 +1,40 @@
+from fpdf import FPDF
 import requests
+from datetime import datetime
 
-# Function to retrieve the last receipt data
 def get_last_receipt():
-    response = requests.get("http://localhost:8001/getLastReceipt")
-    if response.status_code == 200:
-        return response.json()
+    url = "http://localhost:8001/getLastReceipt"
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Assuming the response is in JSON format
+            receipt_info = response.json()
+            return receipt_info
+        else:
+            print("Error: Failed to retrieve receipt information. Status code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("Error: Failed to connect to the server:", e)
+    
+    return None
+
+def create_pdf():
+    current_datetime = datetime.now().strftime(f"{'%Y'}-{'%m'}-{'%d'}_{'%H'}-{'%M'}-{'%S'}")
+    receipt_data = get_last_receipt()
+    if receipt_data is not None:
+        pdf = FPDF()
+        pdf.add_page()
+
+        # Assuming receipt_data is a dictionary with relevant information
+        pdf.set_font("Arial", size=12)
+        pdf.cell(0, 10, "Receipt Information", ln=True, align="C")
+
+        for key, value in receipt_data.items():
+            pdf.cell(0, 10, f"{key}: {value}", ln=True)
+
+        pdf.output(f"receipt{current_datetime}.pdf")
+        print("PDF created successfully.")
     else:
-        return None
+        print("Error: Failed to retrieve receipt information.")
 
-# Function to send a POST request to print the last receipt
-def print_last_receipt():
-    response = requests.post("http://localhost:8001/PrintLastReceipt")
-    if response.status_code == 200:
-        print("Print request sent successfully.")
-    else:
-        print("Failed to send print request.")
-
-# Function to create the PDF
-def create_pdf(receipt_data):
-    # Your PDF creation code here
-    # Replace the sample_receipt_data with receipt_data in the function
-
-# Get the last receipt data
-receipt_data = get_last_receipt()
-
-if receipt_data is not None:
-    # Create the PDF using the receipt data
-    create_pdf(receipt_data)
-
-    # Optionally, send a print request
-    print_last_receipt()
-else:
-    print("Failed to retrieve last receipt data.")
+create_pdf()
