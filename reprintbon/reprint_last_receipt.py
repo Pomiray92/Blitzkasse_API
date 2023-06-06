1d9634a07236f16dfebe69aa7569bc9e51ebd203
Somon Gulmamadov <90386011+Pomiray92@users.noreply.github.com>
2023-06-06 14:15:14 +0200

    update/reprint: reprint_last_receipt.exe can handle argument parsing. so we can print multiple time

--- reprintbon/reprint_last_receipt.py
+++ reprintbon/reprint_last_receipt.py
@@ -1,15 +1,22 @@
 import requests
+import argparse
 
-def reprint_last_receipt():
+def reprint_last_receipt(reprint_count):
     reprint_receipt_url = "http://localhost:8001/ReprintLastReceipt/"
     try:
-        response = requests.get(reprint_receipt_url)
-        if response.status_code == 200:
-            print("Last receipt printed successfully!")
-        else:
-            print("Failed to print last receipt. Status Code:", response.status_code)
+        for i in range(1, reprint_count + 1):
+            response = requests.get(reprint_receipt_url)
+            if response.status_code == 200:
+                print(f"Receipt {i}/{reprint_count} printed successfully!")
+            else:
+                print("Failed to print last receipt. Status Code:", response.status_code)
     except requests.exceptions.RequestException as e:
         print("Error:", e)
 
-# Call the reprint_last_receipt() function
-reprint_last_receipt()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Reprint Last Receipt")
+    parser.add_argument("reprint_count", nargs="?", type=int, default=1, help="Number of times to reprint the receipt")
+    args = parser.parse_args()
+
+    reprint_count = args.reprint_count
+    reprint_last_receipt(reprint_count)
