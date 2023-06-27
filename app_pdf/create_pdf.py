e688b29790b7cdf7d9fc062760f65bbb296b6df6
Somon Gulmamadov <90386011+Pomiray92@users.noreply.github.com>
2023-06-27 10:26:22 +0200

    main/app_pdf: providing the writh path and a correct name for the pdf

--- app_pdf/create_pdf.py
+++ app_pdf/create_pdf.py
@@ -1,8 +1,13 @@
 import pdfkit
+from datetime import datetime
+from get_api import data_dict
+
+timestamp = datetime.now().strftime("%d.%m.%Y_%H-%M-%S")
+receipt_number = data_dict["BONNUMBER"]
 
 config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
 
 html_file = r'C:\Users\POS\Desktop\somon\Blitzkasse_API\app_pdf\templates\rendered_template.html'
-pdf_file = r'C:\Users\POS\Desktop\somon\Blitzkasse_API\app_pdf\file.pdf'
+pdf_file = rf'C:\Users\POS\Desktop\somon\Blitzkasse_API\app_pdf\data_pdf\Rechnung_N_{receipt_number}_{timestamp}.pdf'
 
 pdfkit.from_file(html_file, pdf_file, configuration=config)
